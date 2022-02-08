bb48b8c21f770a2c39975c71382db481e4f96ad0
Baier,Tom (HR Appr) BIP-DE-I <tom.baier@boehringer-ingelheim.com>
2022-02-08 10:33:07 +0100

    feed.tsx added

---
+++ src/pages/Feed.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+import { Box, Button, makeStyles, TextField, Grid, Paper, styled } from '@material-ui/core'
+
+
+const Feed = () => {
+    return(
+        <Box>
+            <Typography color="inherit" align="center">
+                
+            </Typography>
+        </Box>
+    )
+}
+
+export default Feed;
