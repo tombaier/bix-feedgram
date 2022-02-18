e6699e2cfc77f6be84bf44eaa2fc2105705f9f16
Baier,Tom (HR Appr) BIP-DE-I <tom.baier@boehringer-ingelheim.com>
2022-02-18 15:22:19 +0100

    posts.tsx integrated

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,37 +1,59 @@
-import { Card, CardHeader, CardMedia, CardContent, CardActions, Avatar, IconButton, Typography } from '@mui/material'
+import { Card, CardHeader, CardMedia, CardContent, CardActions, Avatar, IconButton, Typography, Grid } from '@mui/material'
 import { Favorite, Comment } from '@mui/icons-material' 
+import { makeStyles } from '@mui/styles'
 
+export interface IPostProps {
+	username: string,
+	caption: string,
+    imageUrl: string,
+};
 
-const Post = () => {
+const useStyles = makeStyles({
+    post_img: {
+        width: '100%',
+        objectFit: 'contain',
+    },
+    post_text:{
+        fontWeight: 'normal',
+        textAlign: 'left',
+    }
+})
 
-  return (
-      <Card>
-          <CardHeader 
-            avatar = {<Avatar />}
-            title = 'UserName'
-            subheader = {new Date().toDateString()}
-          />
+export const Post = (props: IPostProps) => {
+    const classes = useStyles();
+    return (
+        <Grid container>
+            <Grid item container xs={12} justifyContent='center'>
+                <Grid item xs={10} sx={{paddingBottom:'20px'}}> 
+                    <Card>
+                        <CardHeader 
+                            avatar = {<Avatar />}
+                            title = {props.username}
+                            subheader = {new Date().toDateString()}
+                        />
 
-          <CardMedia />
+                        <CardMedia>
+                            <img className={classes.post_img} src={props.imageUrl} />
+                        </CardMedia>
 
-          <CardActions disableSpacing>
-              <IconButton>
-                    <Favorite />
-              </IconButton>
-              
-              <IconButton>
-                    <Comment />
-              </IconButton>
-          
-          </CardActions>
+                        <CardActions disableSpacing sx={{paddingBottom:'1px'}}>
+                            <IconButton>
+                                <Favorite />
+                            </IconButton>
+                        
+                            <IconButton>
+                                    <Comment />
+                            </IconButton>
+                        </CardActions>
 
-          <CardContent>
-              <Typography variant='body2' color='textSecondary' component='p'>
-                  This is the first post on FEEDGRAM! Welcome to you all!
-              </Typography>
-          </CardContent>
-      </Card>
+                        <CardContent sx={{paddingBlockStart:'1px'}}>
+                            <Typography color='textSecondary' component='div'>
+                                <h4 className={classes.post_text}> <strong>{props.username}</strong> {props.caption} </h4>
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </ Grid>
+            </ Grid>
+        </ Grid>
     )
 }
-
-export default Post;
