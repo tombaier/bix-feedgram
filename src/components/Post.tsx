1f3bc369d9954ddc2665ef9456da38f0b7bd2f60
Baier,Tom (HR Appr) BIP-DE-I <tom.baier@boehringer-ingelheim.com>
2022-02-18 11:15:36 +0100

    post.tsx improved

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,29 +1,18 @@
 import { Card, CardHeader, CardMedia, CardContent, CardActions, Avatar, IconButton, Typography } from '@mui/material'
-import { Favorite, Comment, Share, MoreVert } from '@mui/icons-material' 
-import { makeStyles } from '@mui/styles'
+import { Favorite, Comment } from '@mui/icons-material' 
 
-const useStyles = makeStyles({
-    media: {
-        height: 0,
-        paddingTop: '100%'
-    }
-})
 
 const Post = () => {
-  const {media} = useStyles()  
+
   return (
       <Card>
           <CardHeader 
             avatar = {<Avatar />}
-            title = 'Just a Test'
+            title = 'UserName'
             subheader = {new Date().toDateString()}
-            action = {
-                <IconButton>
-                    <MoreVert />
-                </IconButton> 
-            }
           />
-          <CardMedia className={media} />
+
+          <CardMedia />
 
           <CardActions disableSpacing>
               <IconButton>
@@ -34,9 +23,6 @@ const Post = () => {
                     <Comment />
               </IconButton>
           
-              <IconButton>
-                    <Share />
-              </IconButton>
           </CardActions>
 
           <CardContent>
