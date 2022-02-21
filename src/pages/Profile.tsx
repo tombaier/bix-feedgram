41d38845f42f0a95f14196397970d22e9b2638bb
Baier,Tom (HR Appr) BIP-DE-I <tom.baier@boehringer-ingelheim.com>
2022-02-21 15:11:21 +0100

    loading removed, fetch user added

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -6,13 +6,32 @@ import { Logout } from '@mui/icons-material'
 import IconButton from '@mui/material/IconButton';
 import { Link } from 'react-router-dom'
 import { HeaderMain } from '../components/HeaderMain'
-import { auth, logout } from '../services/firebase'
-import { useState } from 'react'
+import { auth, db, logout } from '../services/firebase'
+import { useEffect, useState } from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
+import { query, collection, getDocs, where } from 'firebase/firestore'
+import { useNavigate } from 'react-router-dom'
 
 const Profile = () => {
-    const [user, loading, error] = useAuthState(auth);
+    const [user, error] = useAuthState(auth);
     const [name, setName] = useState("");
+    const navigate = useNavigate();
+    const fetchUserName = async () => {
+        try {
+            const q = query(collection(db, "users"), where("uid", "==", user?.uid));
+            const results = await getDocs(q);
+            const data = results.docs[0].data();
+            setName(data.name);
+        } catch (err) {
+            console.error(err);
+            alert ("An error occurred while fetching user data");
+        }
+    };
+
+    useEffect (() => {
+        if (!user) return navigate("/login");
+        fetchUserName();
+    }, [user]);
     
     return(
         <>
