ac8f8c852d3d6dd57282fd987d06999a275db960
Baier,Tom (HR Appr) BIP-DE-I <tom.baier@boehringer-ingelheim.com>
2022-02-25 15:56:10 +0100

    Navigation improved

--- src/components/HeaderMain.tsx
+++ src/components/HeaderMain.tsx
@@ -2,31 +2,35 @@ import Avatar from '@mui/material/Avatar'
 import { HeaderBase } from './HeaderBase'
 import { SearchBar } from './SearchBar'
 import { MenuBase } from './MenuBase';
-import { Link } from 'react-router-dom';
-import { MenuItem } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { MenuItem, MenuList } from '@mui/material';
 import { logout } from '../services/firebase';
 
 
 export const HeaderMain = () => {
+  const navigate = useNavigate();
+
+  const onFeed = () => {
+    navigate("/feed")
+  }
+
+  const onProfile = () => {
+    navigate("/profile")
+  }
 
   return (
     <HeaderBase>
       <SearchBar/>
       <MenuBase 
         menuIcon={<Avatar sx={{ width: 40, height: 40 }} />} 
-        menuContent={<>
-          <Link to='/profile'>
-            <MenuItem> Profile </MenuItem>
-          </Link>
-          <Link to='/feed'>
-            <MenuItem> Feed </MenuItem>
-          </Link>
-          <MenuItem
-            onClick={logout}
-          > 
-            Logout 
-          </MenuItem>
-        </>} />
+        menuContent={
+          <MenuList>
+            <MenuItem onClick={onProfile}> Profile </MenuItem>
+            <MenuItem onClick={onFeed}> Feed </MenuItem>
+            <MenuItem onClick={logout} > Logout </MenuItem>
+          </MenuList>
+        } 
+      />
     </HeaderBase>
   )
 };
