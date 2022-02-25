d09ba2cd95d984e3e7e43bf566f7011dee362124
Baier,Tom (HR Appr) BIP-DE-I <tom.baier@boehringer-ingelheim.com>
2022-02-25 11:32:53 +0100

    return Alert improved

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,32 +1,15 @@
 import Alert, { AlertProps } from "@mui/material/Alert"
-import Box from "@mui/material/Box"
-import { Center } from "./Center"
 
-export interface IMessageProps {
-	messageSeverity: boolean,
-	messageContent: string
-};
 
-export const Message = (props: IMessageProps) => (
-  <Center sx={{marginTop: '10px'}}>
-    <Box
-      style={{
-        display: 'flex',
-        verticalAlign: 'text-bottom',
-        boxSizing: 'inherit',
-        justifyContent: 'center',
-        textAlign: 'center',
-        alignItems: 'center',
-        width: 300,
-      }}
-    >
-      <Alert
-
-        severity= {props.messageSeverity ? 'success' : 'error'}
-      >
-        {props.messageContent} 
-      </Alert>
-      
-    </Box>
-  </Center>
-) 
+export const Message = ({severity = "error", children, ...props}: AlertProps) => {
+  return <Alert 
+    sx = {{
+      marginTop: '10px',
+      width: '300px',
+    }}
+    severity = {severity}
+    {...props}
+  >
+    {children}
+  </Alert>
+}
