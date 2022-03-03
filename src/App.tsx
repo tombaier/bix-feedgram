010db19ff783d39f6395e7d90c05481adb279538
Baier,Tom (HR Appr) BIP-DE-I <tom.baier@boehringer-ingelheim.com>
2022-03-03 15:40:09 +0100

    router.tsx added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,8 @@
-import Login from './pages/Login'
-import Signup from './pages/Signup'
-import Feed from './pages/Feed'
-import Profile from './pages/Profile'
-import Reset from './pages/Reset'
-import AddPost from './features/posts/AddPost'
-import { Route, Routes } from 'react-router-dom'
+import { Router } from './Router'
 
 const App = () => {
   return (
-      <div>
-        <Routes>
-          <Route path='' element={<Feed />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/feed' element={<Feed />} />
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/reset' element={<Reset />} />
-          <Route path='/addPost' element={<AddPost />} />
-        </Routes>
-      </div>
+    <Router />
   )    
 }
 
