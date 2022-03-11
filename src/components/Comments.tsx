e6a58c5cde514ca79a382a92f5d8fe2006a8ccd1
Baier,Tom (HR Appr) BIP-DE-I <tom.baier@boehringer-ingelheim.com>
2022-03-11 16:06:58 +0100

    layout for displaying comments

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,4 +1,7 @@
+import { Send } from '@mui/icons-material';
 import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import { addDoc, collection, onSnapshot, Timestamp } from 'firebase/firestore';
 import { ChangeEvent, useState } from 'react';
@@ -15,13 +18,25 @@ interface ICommentProps {
 const Comments = (props: ICommentProps) => {
 
     return(
-        <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-            <Typography color='textSecondary' component='div'>
-                <strong>{props.user}</strong> {props.content} <em>{props.date}</em>
-            </Typography>
-        </Box>
+        <>
+            <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                <Typography color='textSecondary' component='div'>
+                    <strong>{props.user}</strong> {props.content} <em>{props.date}</em>
+                </Typography>
+            </Box>
+            <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                <TextField 
+                    id="addComment"  
+                    variant="standard" 
+                    label="Add comment..." 
+                    style = {{width: 250}}
+                />
+                <IconButton>
+                    <Send />
+                </IconButton>
+            </Box>
+        </>
     )
 }
-
 
 export default Comments;
